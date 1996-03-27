1c71fc34233261dc7f45ab63faab77ce9b846386
wessels <wessels>
1996-03-27 20:21:15 +0000

    create setMaxFD() in tools.c

--- src/tools.h
+++ src/tools.h
@@ -1,5 +1,5 @@
 
-/* $Id: tools.h,v 1.5 1996/03/27 18:51:00 wessels Exp $ */
+/* $Id: tools.h,v 1.6 1996/03/27 20:21:15 wessels Exp $ */
 
 extern char *getMyHostname _PARAMS((void));
 extern int getMaxFD _PARAMS((void));
@@ -13,5 +13,6 @@ extern void sig_child _PARAMS((int sig));
 extern void check_suid _PARAMS((void));
 extern int daemonize _PARAMS((void));
 extern void writePidFile _PARAMS((void));
+extern void setMaxFD _PARAMS((void));
 
 extern int do_mallinfo;
