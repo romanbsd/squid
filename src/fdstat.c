cd43a2b02fa5a8f4b9e5472e573ac98034dbcdd9
wessels <wessels>
1996-03-29 21:19:28 +0000

    assign various debug sections

--- src/fdstat.c
+++ src/fdstat.c
@@ -1,5 +1,7 @@
 
-/* $Id: fdstat.c,v 1.6 1996/03/28 22:37:55 wessels Exp $ */
+/* $Id: fdstat.c,v 1.7 1996/03/29 21:19:18 wessels Exp $ */
+
+/* DEBUG: Section 7             fdstat: */
 
 #include "squid.h"
 
@@ -69,7 +71,7 @@ void fdstat_update(fd, status)
     unsigned int i;
 
     if (fd >= getMaxFD())
-	debug(0, 0, "Running out of file descriptors");
+	debug(7, 0, "Running out of file descriptors");
 
     if (fd < Biggest_FD) {
 	/* nothing to do here */
@@ -94,12 +96,12 @@ void fdstat_update(fd, status)
 	/* it could happen since some of fd are out of our control */
 	return;
     }
-    debug(0, 0, "WARNING: fdstat_update: Internal inconsistency:\n");
-    debug(0, 0, "         Biggest_FD = %d, this fd = %d, status = %s\n",
+    debug(7, 0, "WARNING: fdstat_update: Internal inconsistency:\n");
+    debug(7, 0, "         Biggest_FD = %d, this fd = %d, status = %s\n",
 	Biggest_FD, fd, status == OPEN ? "OPEN" : "CLOSE");
-    debug(0, 0, "         fd_stat_tab[%d].status == %s\n",
+    debug(7, 0, "         fd_stat_tab[%d].status == %s\n",
 	fd, fd_stat_tab[fd].status == OPEN ? "OPEN" : "CLOSE");
-    debug(0, 0, "         fd_stat_tab[%d].type == %s\n", fd,
+    debug(7, 0, "         fd_stat_tab[%d].type == %s\n", fd,
 	fdfiletype(fd_stat_tab[fd].type));
 
     return;
