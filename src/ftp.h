5594485b0386b1a376981a4c14eda63629e50867
wessels <wessels>
1996-04-01 18:22:43 +0000

    prototype for ftpCachable

--- src/ftp.h
+++ src/ftp.h
@@ -1,5 +1,6 @@
 
-/* $Id: ftp.h,v 1.3 1996/03/28 03:02:09 wessels Exp $ */
+/* $Id: ftp.h,v 1.4 1996/04/01 18:22:43 wessels Exp $ */
 
 extern int ftpStart _PARAMS((int unusedfd, char *url, StoreEntry * entry));
 extern int ftpInitialize _PARAMS((void));
+extern int ftpCachable _PARAMS((char *, char *, char *));
