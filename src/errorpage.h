293e4989cef43e7289fc0c4eca81b986ee356e0d
wessels <wessels>
1996-04-04 17:46:40 +0000

    add http_code arg to cached_error_url

--- src/errorpage.h
+++ src/errorpage.h
@@ -1,9 +1,9 @@
 
 
-/* $Id: errorpage.h,v 1.5 1996/04/04 01:30:42 wessels Exp $ */
+/* $Id: errorpage.h,v 1.6 1996/04/04 17:46:39 wessels Exp $ */
 
 void cached_error_entry _PARAMS((StoreEntry *, int, char *));
-char *cached_error_url _PARAMS((char *, int, int, char *, char *));
-char *cached_error_request _PARAMS((char *, int, char *));
+char *cached_error_url _PARAMS((char *, int, int, char *, int, char *));
+char *cached_error_request _PARAMS((char *, int, char *, int));
 
 extern char tmp_error_buf[];
