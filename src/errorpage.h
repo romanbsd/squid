d47fe2c021c631c5dad1a5ecbe9557bd635e018f
wessels <wessels>
1996-04-05 23:21:12 +0000

    make sure tmp_error_buf is big enough to hold two URLs use pointers rather than arrays

--- src/errorpage.h
+++ src/errorpage.h
@@ -1,9 +1,10 @@
 
 
-/* $Id: errorpage.h,v 1.6 1996/04/04 17:46:39 wessels Exp $ */
+/* $Id: errorpage.h,v 1.7 1996/04/05 23:21:11 wessels Exp $ */
 
-void cached_error_entry _PARAMS((StoreEntry *, int, char *));
-char *cached_error_url _PARAMS((char *, int, int, char *, int, char *));
-char *cached_error_request _PARAMS((char *, int, char *, int));
+extern void cached_error_entry _PARAMS((StoreEntry *, int, char *));
+extern char *cached_error_url _PARAMS((char *, int, int, char *, int, char *));
+extern char *cached_error_request _PARAMS((char *, int, char *, int));
+extern void errorInitialize _PARAMS((void));
 
-extern char tmp_error_buf[];
+extern char *tmp_error_buf;
