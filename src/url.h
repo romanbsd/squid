e74ee99cf010f8d5e4474c6026b66f4c8da2bdd8
wessels <wessels>
1996-04-10 03:54:00 +0000

    make url_convert_hex not have to xstrdup the buffer

--- src/url.h
+++ src/url.h
@@ -1,11 +1,11 @@
-/*  $Id: url.h,v 1.2 1996/03/27 01:46:29 wessels Exp $ */
+/*  $Id: url.h,v 1.3 1996/04/10 03:54:00 wessels Exp $ */
 
 #ifndef _URL_HEADER_
 #define _URL_HEADER_
 
 #define MAX_URL  (ICP_MAX_URL)
 
-extern char *url_convert_hex _PARAMS((char *org_url));
+extern char *url_convert_hex _PARAMS((char *org_url, int allocate));
 extern char *url_escape _PARAMS((char *url));
 
 #endif /* _URL_HEADER_ */
