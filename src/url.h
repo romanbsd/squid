1c2308fafb8ef6e62ef7d3a4c31e339af6057d11
wessels <wessels>
1996-04-11 22:52:33 +0000

    finish up ACL implementation

--- src/url.h
+++ src/url.h
@@ -1,11 +1,36 @@
-/*  $Id: url.h,v 1.3 1996/04/10 03:54:00 wessels Exp $ */
+/*  $Id: url.h,v 1.4 1996/04/11 22:52:33 wessels Exp $ */
 
 #ifndef _URL_HEADER_
 #define _URL_HEADER_
+
+typedef enum {
+    METHOD_NONE,
+    METHOD_GET,
+    METHOD_POST,
+    METHOD_HEAD
+} method_t;
+
+extern char *RequestMethodStr[];
+
+typedef enum {
+    PROTO_NONE,
+    PROTO_HTTP,
+    PROTO_FTP,
+    PROTO_GOPHER,
+    PROTO_WAIS,
+    PROTO_CACHEOBJ,
+    PROTO_MAX
+} protocol_t;
+
+extern char *ProtocolStr[];
+
 
 #define MAX_URL  (ICP_MAX_URL)
 
 extern char *url_convert_hex _PARAMS((char *org_url, int allocate));
 extern char *url_escape _PARAMS((char *url));
+extern protocol_t urlParseProtocol _PARAMS((char *));
+extern method_t   urlParseMethod _PARAMS((char *));
+extern int urlDefaultPort _PARAMS((protocol_t));
 
 #endif /* _URL_HEADER_ */
