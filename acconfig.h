b1271c6e1f0e051205bcf1120ce38685d5f397c8
wessels <wessels>
1996-04-15 18:00:28 +0000

    change check for extended mallinfo structure

--- acconfig.h
+++ acconfig.h
@@ -1,4 +1,8 @@
-/* $Id: acconfig.h,v 1.1 1996/04/15 03:50:48 wessels Exp $ */
+/* $Id: acconfig.h,v 1.2 1996/04/15 18:00:25 wessels Exp $ */
 
 /* Define struct tm has tm_gmtoff member */
 #undef HAVE_TM_GMTOFF
+
+/* Define struct mallinfo has mxfast member */
+#undef HAVE_EXT_MALLINFO
+
