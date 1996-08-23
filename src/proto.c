876dac77e42bb6f722a566da513c35af9b63044f
wessels <wessels>
1996-08-23 21:26:54 +0000

    - Changed protoUndispatch() to never call squid_error_entry().

--- src/proto.c
+++ src/proto.c
@@ -1,5 +1,5 @@
 /*
- * $Id: proto.c,v 1.44 1996/08/21 20:11:56 wessels Exp $
+ * $Id: proto.c,v 1.45 1996/08/23 21:26:54 wessels Exp $
  *
  * DEBUG: section 17    Neighbor Selection
  * AUTHOR: Harvest Derived
@@ -377,8 +377,10 @@ void protoUnregister(fd, entry, request, src_addr)
     (void) fqdncacheUnregister(src_addr, fd);
     if (host)
 	(void) ipcache_unregister(host, fd);
-    if (entry)
+#ifdef DONT_DO_THIS 
+    if (entry && BIT_SET(entry->flag, CLIENT_ABORT_REQUEST))
 	squid_error_entry(entry, ERR_CLIENT_ABORT, NULL);
+#endif
 }
 
 void protoCancelTimeout(fd, entry)
