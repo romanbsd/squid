2f6d82a79f61e3ced846f74ed424a3ce781d2978
wessels <wessels>
1996-10-11 19:34:37 +0000

    protect log enable/disable with password

--- src/objcache.c
+++ src/objcache.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: objcache.c,v 1.27 1996/10/09 15:34:33 wessels Exp $
+ * $Id: objcache.c,v 1.28 1996/10/11 19:34:37 wessels Exp $
  *
  * DEBUG: section 16    Cache Manager Objects
  * AUTHOR: Harvest Derived
@@ -227,7 +227,6 @@ objcacheStart(int fd, char *url, StoreEntry * entry)
 	    safe_free(buf);
 	    shut_down(0);
 	}
-
     } else if (strcmp(data->request, "info") == 0) {
 	BIT_SET(data->entry->flag, DELAY_SENDING);
 	HTTPCacheInfo->info_get(HTTPCacheInfo, data->entry);
@@ -309,17 +308,33 @@ objcacheStart(int fd, char *url, StoreEntry * entry)
 	storeComplete(data->entry);
 
     } else if (strcmp(data->request, "log/enable") == 0) {
-	BIT_SET(data->entry->flag, DELAY_SENDING);
-	HTTPCacheInfo->log_enable(HTTPCacheInfo, data->entry);
-	BIT_RESET(data->entry->flag, DELAY_SENDING);
-	storeComplete(data->entry);
-
+	if (objcache_CheckPassword(password, username) != 0) {
+	    buf = xstrdup(BADPassword);
+	    storeAppendPrintf(data->entry, buf);
+	    storeAbort(data->entry, "SQUID:OBJCACHE Incorrect Password\n");
+	    /* override negative TTL */
+	    data->entry->expires = squid_curtime + STAT_TTL;
+	    debug(16, 1, "Attempt to log/enable with incorrect password\n");
+	} else {
+	    BIT_SET(data->entry->flag, DELAY_SENDING);
+	    HTTPCacheInfo->log_enable(HTTPCacheInfo, data->entry);
+	    BIT_RESET(data->entry->flag, DELAY_SENDING);
+	    storeComplete(data->entry);
+	}
     } else if (strcmp(data->request, "log/disable") == 0) {
-	BIT_SET(data->entry->flag, DELAY_SENDING);
-	HTTPCacheInfo->log_disable(HTTPCacheInfo, data->entry);
-	BIT_RESET(data->entry->flag, DELAY_SENDING);
-	storeComplete(data->entry);
-
+	if (objcache_CheckPassword(password, username) != 0) {
+	    buf = xstrdup(BADPassword);
+	    storeAppendPrintf(data->entry, buf);
+	    storeAbort(data->entry, "SQUID:OBJCACHE Incorrect Password\n");
+	    /* override negative TTL */
+	    data->entry->expires = squid_curtime + STAT_TTL;
+	    debug(16, 1, "Attempt to log/disable with incorrect password\n");
+	} else {
+	    BIT_SET(data->entry->flag, DELAY_SENDING);
+	    HTTPCacheInfo->log_disable(HTTPCacheInfo, data->entry);
+	    BIT_RESET(data->entry->flag, DELAY_SENDING);
+	    storeComplete(data->entry);
+	}
     } else if (strcmp(data->request, "log/clear") == 0) {
 	BIT_SET(data->entry->flag, DELAY_SENDING);
 	HTTPCacheInfo->log_clear(HTTPCacheInfo, data->entry);
