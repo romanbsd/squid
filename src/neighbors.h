3ea57759a6c02e6424aeda8146684e26c78a94f0
wessels <wessels>
1996-10-19 07:10:09 +0000

    make neighborAlive static

--- src/neighbors.h
+++ src/neighbors.h
@@ -1,5 +1,5 @@
 /*
- * $Id: neighbors.h,v 1.21 1996/10/19 07:08:36 wessels Exp $
+ * $Id: neighbors.h,v 1.22 1996/10/19 07:10:09 wessels Exp $
  *
  * AUTHOR: Harvest Derived
  *
@@ -219,7 +219,6 @@ extern void neighbors_init _PARAMS((void));
 extern void neighbors_open _PARAMS((int));
 extern void neighborsDestroy _PARAMS((void));
 extern edge *neighborFindByName _PARAMS((char *));
-extern void neighborAlive _PARAMS((edge *, MemObject *, icp_common_t *));
 
 extern char *hier_strings[];
 
