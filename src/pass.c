67f8e9b135667afd59f46cc9886e7e261a8883c4
wessels <wessels>
1997-10-24 18:10:41 +0000

    err->errno --> err->xerrno

--- src/pass.c
+++ src/pass.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: pass.c,v 1.55 1997/08/10 04:42:43 wessels Exp $
+ * $Id: pass.c,v 1.56 1997/10/24 18:10:40 wessels Exp $
  *
  * DEBUG: section 39    HTTP Passthrough
  * AUTHOR: Duane Wessels
@@ -316,7 +316,7 @@ passConnectDone(int fd, int status, void *data)
 	err = xcalloc(1, sizeof(ErrorState));
 	err->type = ERR_CONNECT_FAIL;
 	err->http_status = HTTP_SERVICE_UNAVAILABLE;
-	err->errno = errno;
+	err->xerrno = errno;
 	err->host = xstrdup(passState->host);
 	err->port = passState->port;
 	err->request = requestLink(request);
@@ -378,7 +378,7 @@ passStart(int fd, const char *url, request_t * request, size_t * size_ptr)
 	err = xcalloc(1, sizeof(ErrorState));
 	err->type = ERR_SOCKET_FAILURE;
 	err->http_status = HTTP_INTERNAL_SERVER_ERROR;
-	err->errno = errno;
+	err->xerrno = errno;
 	err->request = requestLink(request);
 	errorSend(fd, err);
 	return;
