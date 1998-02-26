80db76076d1ee43d82ba52777c996cb87e0f4902
kostas <kostas>
1998-02-26 22:16:31 +0000

    Fixed snmp memleaks

--- src/snmp.c
+++ src/snmp.c
@@ -1,5 +1,5 @@
 /*
- * $Id: snmp.c,v 1.40 1998/02/26 18:00:50 wessels Exp $
+ * $Id: snmp.c,v 1.41 1998/02/26 22:16:30 kostas Exp $
  *
  * DEBUG: section 49    SNMP support
  * AUTHOR: Kostas Anagnostakis
@@ -214,6 +214,8 @@ snmp_agent_parse_done(int errstat, snmp_request_t * snmp_rq)
 	if (snmpFwd_removePending(&snmp_rq->from, this_reqid)) {	/* failed */
 	    debug(49, 2) ("snmp: bogus response from %s.\n",
 		inet_ntoa(snmp_rq->from.sin_addr));
+		if (snmp_rq->community)
+			xfree(snmp_rq->community);
 	    xfree(snmp_rq->outbuf);
 	    xfree(snmp_rq);
 	    return;
