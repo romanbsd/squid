af7ed00835230cc09a4d40ee4961b341c06fbbf3
kostas <kostas>
1998-02-26 23:18:14 +0000

    Added mib version check and ignore -DEFINITIONS so that we don't need to have this stuff commented in mib.txt

--- src/snmp.c
+++ src/snmp.c
@@ -1,5 +1,5 @@
 /*
- * $Id: snmp.c,v 1.41 1998/02/26 22:16:30 kostas Exp $
+ * $Id: snmp.c,v 1.42 1998/02/26 23:18:14 kostas Exp $
  *
  * DEBUG: section 49    SNMP support
  * AUTHOR: Kostas Anagnostakis
@@ -277,9 +277,9 @@ snmpInit(void)
     init_mib(Config.Snmp.mibPath);
 #endif
     if (!Config.Snmp.communities)
-	debug(49, 5) ("snmpInit: communities not defined yet !\n");
+	debug(49, 2) ("snmpInit: WARNING:communities not defined yet!\n");
     else
-	debug(49, 5) ("snmpInit: well, well , communities defined!\n");
+	debug(49, 5) ("snmpInit: communities defined.\n");
     if (default_auth() < 0)
 	exit(2);
 
