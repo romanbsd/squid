37dc3e113efe6c84d9b127e5fd7de459a8ecda96
hno <hno>
2002-11-10 17:08:55 +0000

    Use POST when submitting the login to prevent the login & password from being logged in the HTTP server access log or the browser history.

--- src/cachemgr.c
+++ src/cachemgr.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: cachemgr.c,v 1.99 2002/09/01 12:37:46 hno Exp $
+ * $Id: cachemgr.c,v 1.100 2002/11/10 17:08:55 hno Exp $
  *
  * DEBUG: section 0     CGI Cache Manager
  * AUTHOR: Duane Wessels
@@ -242,7 +242,7 @@ auth_html(const char *host, int port, const char *user_name)
     printf("<P>This is a WWW interface to the instrumentation interface\n");
     printf("for the Squid object cache.</P>\n");
     printf("<HR noshade size=\"1px\">\n");
-    printf("<FORM METHOD=\"GET\" ACTION=\"%s\">\n", script_name);
+    printf("<FORM METHOD=\"POST\" ACTION=\"%s\">\n", script_name);
     printf("<TABLE BORDER=\"0\" CELLPADDING=\"10\" CELLSPACING=\"1\">\n");
     printf("<TR><TH ALIGN=\"left\">Cache Host:</TH><TD><INPUT NAME=\"host\" ");
     printf("size=\"30\" VALUE=\"%s\"></TD></TR>\n", host);
