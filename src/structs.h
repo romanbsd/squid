a307cb212b8b197b760e46cba1e72885c8956077
hno <hno>
2006-05-22 22:24:09 +0000

    Resolved merge conflict between pinning and external acl grace period patches

--- src/structs.h
+++ src/structs.h
@@ -1,6 +1,6 @@
 
 /*
- * $Id: structs.h,v 1.456 2006/05/22 22:16:37 hno Exp $
+ * $Id: structs.h,v 1.457 2006/05/22 22:24:09 hno Exp $
  *
  *
  * SQUID Web Proxy Cache          http://www.squid-cache.org/
@@ -1820,7 +1820,6 @@ struct _request_t {
     struct in_addr client_addr;
     struct in_addr my_addr;
     unsigned short my_port;
-    unsigned short client_port;
     HttpHeader header;
     squid_off_t content_length;
     HierarchyLogEntry hier;
