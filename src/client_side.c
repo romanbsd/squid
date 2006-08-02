7999c0b61ab2e763530dc23591e542a8cb4b7051
hno <hno>
2006-08-02 21:45:23 +0000

    Bug #1716: Fails to compile with OpenSSL 0.9.7j

--- src/client_side.c
+++ src/client_side.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: client_side.c,v 1.666 2006/07/23 21:44:22 hno Exp $
+ * $Id: client_side.c,v 1.667 2006/08/02 21:45:23 hno Exp $
  *
  * DEBUG: section 33    Client-side Routines
  * AUTHOR: Duane Wessels
@@ -4628,7 +4628,7 @@ clientNegotiateSSL(int fd, void *data)
 	    /* Write out the SSL session details.. actually the call below, but
 	     * OpenSSL headers do strange typecasts confusing GCC.. */
 	    /* PEM_write_SSL_SESSION(debug_log, SSL_get_session(ssl)); */
-#if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x0090708FL
+#if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x00908000L
 	    PEM_ASN1_write((i2d_of_void *) i2d_SSL_SESSION, PEM_STRING_SSL_SESSION, debug_log, (char *) SSL_get_session(ssl), NULL, NULL, 0, NULL, NULL);
 #else
 	    PEM_ASN1_write(i2d_SSL_SESSION, PEM_STRING_SSL_SESSION, debug_log, (char *) SSL_get_session(ssl), NULL, NULL, 0, NULL, NULL);
