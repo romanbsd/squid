5cfd635baf072f3b8e98e0ee202718e1d8cd0538
adrian <adrian>
2006-09-22 00:51:43 +0000

    Fix typo!

--- src/fs/diskd/store_dir_diskd.c
+++ src/fs/diskd/store_dir_diskd.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store_dir_diskd.c,v 1.84 2006/09/08 19:41:25 serassio Exp $
+ * $Id: store_dir_diskd.c,v 1.85 2006/09/22 00:51:43 adrian Exp $
  *
  * DEBUG: section 47    Store Directory Routines
  * AUTHOR: Duane Wessels
@@ -1811,7 +1811,7 @@ storeDiskdDirCheckLoadAv(SwapDir * SD, store_op_t op)
 {
     diskdinfo_t *diskdinfo = SD->fsdata;
     /* Calculate the storedir load relative to magic2 on a scale of 0 .. 1000 */
-    /* the parse function guarantees magic2 is positivie */
+    /* the parse function guarantees magic2 is positive */
     if (diskdinfo->away >= diskdinfo->magic1)
 	return -1;
     return DISKD_LOAD_BASE + (diskdinfo->away * DISKD_LOAD_QUEUE_WEIGHT / diskdinfo->magic2);
