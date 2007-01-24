8dff6c2c8e4b6c14a82ab986cd7dccd340908826
adrian <adrian>
2007-01-24 08:03:52 +0000

    Modify the script slightly to allow the user to override the aggregation term. BY default its "username" but there are other options (eg try "clientip").

--- scripts/PerUser.pl
+++ scripts/PerUser.pl
@@ -7,19 +7,26 @@ use strict;
 # statistics.
 #
 # Adrian Chadd <adrian@squid-cache.org>
-# $Id: PerUser.pl,v 1.1 2007/01/24 07:01:36 adrian Exp $
+# $Id: PerUser.pl,v 1.2 2007/01/24 08:03:52 adrian Exp $
 
 use Squid::ParseLog;
 
 my %u;
+my $wh;
+
+$wh = "username";
+if (scalar @ARGV >= 1) {
+	$wh = $ARGV[0];
+	shift @ARGV;
+}
 
 while (<>) {
 	chomp;
 	my $l = Squid::ParseLog::parse($_);
-	if (! defined $u{$l->{"username"}}) {
-		$u{$l->{"username"}}->{"traffic"} = 0;
+	if (! defined $u{$l->{$wh}}) {
+		$u{$l->{$wh}}->{"traffic"} = 0;
 	}
-	$u{$l->{"username"}}->{"traffic"} += $l->{"size"};
+	$u{$l->{$wh}}->{"traffic"} += $l->{"size"};
 }
 
 foreach (keys %u) {
