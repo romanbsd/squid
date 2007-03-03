62d5e01c4a2c065f1f628640bbdffae1027bc23d
hno <hno>
2007-03-03 18:27:22 +0000

    Author: serassio
    MFC: Cosmetic Fix: use the correct helper name in debug messages
    
    Merged changes:
    2007/03/03 17:22:04 serassio +10 -10   Cosmetic Fix: use the correct helper name in debug messages

--- helpers/ntlm_auth/fakeauth/ntlm.h
+++ helpers/ntlm_auth/fakeauth/ntlm.h
@@ -1,5 +1,5 @@
 /*
- * $Id: ntlm.h,v 1.7 2006/08/28 09:19:29 serassio Exp $
+ * $Id: ntlm.h,v 1.7.2.1 2007/03/03 18:27:21 hno Exp $
  *
  * AUTHOR: Andrew Doran <ad@interlude.eu.org>
  *
@@ -121,7 +121,7 @@ extern int fail_debug_enabled;
 #include <unistd.h>
 static const char *__foo;
 #define debug(X...) if (debug_enabled) { \
-                    fprintf(stderr,"ntlm-auth[%ld](%s:%d): ", (long)getpid(), \
+                    fprintf(stderr,"fakeauth_auth[%ld](%s:%d): ", (long)getpid(), \
                     ((__foo=strrchr(__FILE__,'/'))==NULL?__FILE__:__foo+1),\
                     __LINE__);\
                     fprintf(stderr,X); }
@@ -142,7 +142,7 @@ debug(char *format,...)
 	va_list args;
 
 	va_start(args, format);
-	fprintf(stderr, "ntlm-auth[%ld]: ", (long)getpid());
+	fprintf(stderr, "fakeauth_auth[%ld]: ", (long)getpid());
 	vfprintf(stderr, format, args);
 	va_end(args);
 #if FAIL_DEBUG
