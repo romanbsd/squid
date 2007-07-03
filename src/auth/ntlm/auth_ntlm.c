0d05b8a036f086927b89dd72d4874a333f551beb
hno <hno>
2007-07-03 23:31:51 +0000

    Bug #2008: Work around clients trying to use NTLM or Negotiate without persistent connections
    This patch forces a fallback on Basic/Digest if the client attempts
    to use NTLM or Negotiate without having a persistent connection.

--- src/auth/ntlm/auth_ntlm.c
+++ src/auth/ntlm/auth_ntlm.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: auth_ntlm.c,v 1.38 2007/02/27 22:39:50 hno Exp $
+ * $Id: auth_ntlm.c,v 1.39 2007/07/03 23:31:51 hno Exp $
  *
  * DEBUG: section 29    NTLM Authenticator
  * AUTHOR: Robert Collins
@@ -302,6 +302,8 @@ authenticateNTLMFixErrorHeader(auth_user_request_t * auth_user_request, HttpRepl
 {
     ntlm_request_t *ntlm_request;
     if (!ntlmConfig->authenticate)
+	return;
+    if (!request->flags.proxy_keepalive && request->flags.must_keepalive)
 	return;
     /* New request, no user details */
     if (auth_user_request == NULL) {
@@ -657,6 +659,13 @@ authenticateNTLMAuthenticateUser(auth_user_request_t * auth_user_request, reques
     if (!conn) {
 	ntlm_request->auth_state = AUTHENTICATE_STATE_FAILED;
 	debug(29, 1) ("authenticateNTLMAuthenticateUser: attempt to perform authentication without a connection!\n");
+	return;
+    }
+    if (!request->flags.proxy_keepalive) {
+	debug(29, 2) ("authenticateNTLMAuthenticateUser: attempt to perform authentication without a persistent connection!\n");
+	ntlm_request->auth_state = AUTHENTICATE_STATE_FAILED;
+	request->flags.proxy_keepalive = 0;
+	request->flags.must_keepalive = 1;
 	return;
     }
     if (ntlm_request->waiting) {
