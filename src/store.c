b07cfec360bb24ca84e009fd78bb1f4618320268
hno <hno>
2007-08-31 13:51:27 +0000

    MFC Guard from a potential segmentation fault in storeLocateVary()
    Have gotten a report where Squid crashed with a segmentation fault in
    storeLocateVary under unknown conditions. This patch should protect from it.
    
    Merged changes:
    2007/08/29 21:13:17	hno	+2 -2	Guard from a potential segmentation fault in storeLocateVary()

--- src/store.c
+++ src/store.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: store.c,v 1.570.2.3 2007/03/26 23:14:10 hno Exp $
+ * $Id: store.c,v 1.570.2.4 2007/08/31 13:51:27 hno Exp $
  *
  * DEBUG: section 20    Storage Manager
  * AUTHOR: Harvest Derived
@@ -945,7 +945,7 @@ storeLocateVary(StoreEntry * e, int offset, const char *vary_data, String accept
     state->buf = memAllocBuf(4096, &state->buf_size);
     state->sc = storeClientRegister(state->e, state);
     state->seen_offset = offset;
-    if (strCmp(e->mem_obj->reply->content_type, "x-squid-internal/vary") != 0) {
+    if (!strLen(e->mem_obj->reply->content_type) || strCmp(e->mem_obj->reply->content_type, "x-squid-internal/vary") != 0) {
 	/* This is not our Vary marker object. Bail out. */
 	debug(33, 1) ("storeLocateVary: Not our vary marker object, %s = '%s', '%s'/'%s'\n",
 	    storeKeyText(e->hash.key), e->mem_obj->url, vary_data, strBuf(accept_encoding) ? strBuf(accept_encoding) : "-");
