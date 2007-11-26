c1ed10b89b863fb7bebcd1130e26289c5f1fd3cb
adrian <adrian>
2007-11-26 11:07:27 +0000

    Author: hno Patchset 11750 (HEAD): Improve helper debug ouput, including the channel number

--- src/helper.c
+++ src/helper.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: helper.c,v 1.62.2.3 2007/06/03 00:40:32 hno Exp $
+ * $Id: helper.c,v 1.62.2.4 2007/11/26 11:07:27 adrian Exp $
  *
  * DEBUG: section 84    Helper process maintenance
  * AUTHOR: Harvest Derived?
@@ -1026,7 +1026,7 @@ helperDispatch(helper_server * srv, helper_request * r)
 {
     helper *hlp = srv->parent;
     helper_request **ptr = NULL;
-    int slot;
+    int slot = -1;
     if (!cbdataValid(r->data)) {
 	debug(84, 1) ("helperDispatch: invalid callback data\n");
 	helperRequestFree(r);
@@ -1057,8 +1057,8 @@ helperDispatch(helper_server * srv, helper_request * r)
 	    helperDispatch_done,	/* Handler */
 	    srv);
     }
-    debug(84, 5) ("helperDispatch: Request sent to %s #%d, %d bytes\n",
-	hlp->id_name, srv->index + 1, (int) strlen(r->buf));
+    debug(84, 5) ("helperDispatch: Request sent to %s #%d[%d], %d bytes\n",
+	hlp->id_name, srv->index + 1, slot, (int) strlen(r->buf));
     srv->stats.uses++;
     hlp->stats.requests++;
 }
