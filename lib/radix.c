eff57ac712f7ca6fc9a9c7c871f3def87723477a
hno <hno>
2008-05-04 23:28:44 +0000

    Author: serassio Branch: HEAD Removed advertisement clause from BSD licensed sources
    According to the new revised (3-clause) BSD license:
    ftp://ftp.cs.berkeley.edu/pub/4bsd/README.Impt.License.Change
    the advertisement clause (3) of old (4-clause) BSD license can
    be deleted.

--- lib/radix.c
+++ lib/radix.c
@@ -1,5 +1,5 @@
 /*
- * $Id: radix.c,v 1.21 2006/06/02 17:32:44 serassio Exp $
+ * $Id: radix.c,v 1.21.6.1 2008/05/04 23:28:44 hno Exp $
  *
  * DEBUG: section 53     Radix tree data structure implementation
  * AUTHOR: NetBSD Derived
@@ -45,11 +45,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
