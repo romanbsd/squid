0cd003a9d11d3f7555e1e867a74f91ac806069f7
hno <hno>
2008-07-10 09:07:36 +0000

    Bug #2408: assertion failed: forward.c:529: "fs"
    pinned connections is a bit special and we need to add the alternate
    paths even if it looks the same as a pinned one..

--- src/peer_select.c
+++ src/peer_select.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: peer_select.c,v 1.137 2008/06/22 00:29:36 hno Exp $
+ * $Id: peer_select.c,v 1.138 2008/07/10 09:07:36 hno Exp $
  *
  * DEBUG: section 44    Peer Selection Algorithm
  * AUTHOR: Duane Wessels
@@ -685,7 +685,7 @@ peerAddFwdServer(FwdServer ** FS, peer * p, hier_code code)
 	p ? p->name : "DIRECT",
 	hier_strings[code]);
     while (*FS) {
-	if ((*FS)->peer == p) {
+	if ((*FS)->peer == p && (*FS)->code != PINNED) {
 	    debug(44, 5) ("peerAddFwdServer: Skipping duplicate registration of %s\n", p ? p->name : "DIRECT");
 	    return;
 	}
