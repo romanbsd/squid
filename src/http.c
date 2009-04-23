8952c38cbb4c91cc973d2ae7b965c7799847975f
mnot <mnot>
2009-04-23 01:08:23 +0000

    Author: Mark Nottingham <mnot@pobox.com> Bug #2515: Final chunk parsing errors on FreeBSD6+
    Because strtoll has different behaviour (actually, POSIX-compliant, AFAICT, but whatever)
    on FreeBSD6+, it's not possible to count on strtoll not setting errno to EINVALID when parsing
    "/r/n".
    
    This patch checks to see if it's an empty line before checking errno, avoiding this problem.

--- src/http.c
+++ src/http.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: http.c,v 1.452 2008/10/14 19:04:37 hno Exp $
+ * $Id: http.c,v 1.453 2009/04/23 01:08:23 mnot Exp $
  *
  * DEBUG: section 11    Hypertext Transfer Protocol (HTTP)
  * AUTHOR: Harvest Derived
@@ -745,10 +745,10 @@ httpAppendBody(HttpStateData * httpState, const char *buf, ssize_t len, int buff
 		    debug(11, 3) ("Chunk header '%s'\n", strBuf(httpState->chunkhdr));
 		    errno = 0;
 		    httpState->chunk_size = strto_off_t(strBuf(httpState->chunkhdr), &end, 16);
-		    if (errno)
-			badchunk = 1;
-		    else if (end == strBuf(httpState->chunkhdr))
+		    if (end == strBuf(httpState->chunkhdr))
 			emptychunk = 1;
+		    else if (errno)
+			badchunk = 1;
 		    while (end && (*end == '\r' || *end == ' ' || *end == '\t'))
 			end++;
 		    if (httpState->chunk_size < 0 || badchunk || !end || (*end != '\n' && *end != ';')) {
