570ede0d5bb4765ec4bce076d52a359ded60f984
adrian <adrian>
2009-05-04 05:19:56 +0000

    Merge revision 1.4 from Squid-2.HEAD - Fix Bug #2661.

--- src/comm_devpoll.c
+++ src/comm_devpoll.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: comm_devpoll.c,v 1.3.6.1 2009/02/02 11:10:53 hno Exp $
+ * $Id: comm_devpoll.c,v 1.3.6.2 2009/05/04 05:19:56 adrian Exp $
  *
  * DEBUG: section 5     Socket Functions
  *
@@ -37,8 +37,8 @@
 
 #include <sys/devpoll.h>
 
-#define	DEVPOLL_UPDATESIZE	1024
-#define	DEVPOLL_QUERYSIZE	1024
+#define	DEVPOLL_UPDATESIZE	OPEN_MAX
+#define	DEVPOLL_QUERYSIZE	OPEN_MAX
 
 static int devpoll_fd;
 static struct timespec zero_timespec;
