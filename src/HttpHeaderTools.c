456cd69ce5005bd288bcdb2523250d0ecc954b9c
hno <hno>
2009-08-20 12:05:07 +0000

    Bug #2541: Hang in 100% CPU loop while extacting header details using a delimiter other than comma (external_acl_type, access_log_format, external_refresh_check)
    strListGetItem() could get stuck in a 100% loop if called with a delimiter
    other than ',' and the parsed string contains ','.
    
    This change makes it properly detect ',' as a delimiter even if called
    with another delimiter argument like intended. The reason why ',' is always
    a delimiter is because this is the delimiter between merged values of a
    multi-valued header, and must always be supported as delimiter even if
    Cookie uses ';' instead.

--- src/HttpHeaderTools.c
+++ src/HttpHeaderTools.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: HttpHeaderTools.c,v 1.43 2008/03/31 23:41:12 hno Exp $
+ * $Id: HttpHeaderTools.c,v 1.44 2009/08/20 12:05:07 hno Exp $
  *
  * DEBUG: section 66    HTTP Header Tools
  * AUTHOR: Alex Rousskov
@@ -239,6 +239,10 @@ int
 strListGetItem(const String * str, char del, const char **item, int *ilen, const char **pos)
 {
     size_t len;
+    /* ',' is always enabled as field delimiter as this is required for
+     * processing merged header values properly, even if Cookie normally
+     * uses ';' as delimiter.
+     */
     static char delim[3][8] =
     {
 	"\"?,",
@@ -261,16 +265,15 @@ strListGetItem(const String * str, char del, const char **item, int *ilen, const
     /* find next delimiter */
     do {
 	*pos += strcspn(*pos, delim[quoted]);
-	if (**pos == del)
-	    break;
 	if (**pos == '"') {
 	    quoted = !quoted;
 	    *pos += 1;
-	}
-	if (quoted && **pos == '\\') {
+	} else if (quoted && **pos == '\\') {
 	    *pos += 1;
 	    if (**pos)
 		*pos += 1;
+	} else {
+	    break;		/* Delimiter found, marking the end of this value */
 	}
     } while (**pos);
     len = *pos - *item;		/* *pos points to del or '\0' */
