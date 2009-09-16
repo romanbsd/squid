35d39d46c68428ea83e908218c8dcd8509706c10
hno <hno>
2009-09-16 21:16:03 +0000

    Author: fancyrabbit <fancyrabbit@gmail.com> MFC: Bug 2672: cacheMemMaxSize 32-bit overflow during snmpwalk
    snint is 32-bits. Unwrapped the code casts int64 to int32 before
    reducing the base unit size to MB.

--- src/snmp_agent.c
+++ src/snmp_agent.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: snmp_agent.c,v 1.89 2006/11/09 16:35:07 hno Exp $
+ * $Id: snmp_agent.c,v 1.89.2.1 2009/09/16 21:16:03 hno Exp $
  *
  * DEBUG: section 49     SNMP Interface
  * AUTHOR: Kostas Anagnostakis
@@ -103,7 +103,7 @@ snmp_confFn(variable_list * Var, snint * ErrP)
 	switch (Var->name[LEN_SQ_CONF + 1]) {
 	case CONF_ST_MMAXSZ:
 	    Answer = snmp_var_new_integer(Var->name, Var->name_length,
-		(snint) Config.memMaxSize >> 20,
+		(snint) (Config.memMaxSize >> 20),
 		ASN_INTEGER);
 	    break;
 	case CONF_ST_SWMAXSZ:
